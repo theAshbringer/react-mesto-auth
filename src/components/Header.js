34b2c3b199ce3cbe5d288a0468d1430b3db4faab
theAsbringer <kristya1310@gmail.com>
2022-10-18 23:12:07 +0700

    feat: add different headers  for different routes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './Header.css'
 
-const Header = () => {
+const Header = ({ location }) => {
   return (
     <header className="header">
       <a href="#" className="header__logo" />
+      {location === '/sign-up' && <Link className="header__not-logged-in" to={'/sign-in'}>Войти</Link>}
+      {location === '/sign-in' && <Link className="header__not-logged-in" to={'/sign-up'}>Регистрация</Link>}
+      {location === '/' && (
+        <>
+          <div className="header__logged-in">
+            <p className="header__user">email@mail.com</p>
+            <Link className="header__logout" to={'/sign-in'}>Выйти</Link>
+          </div>
+          <div className='header__burger'></div></>
+      )}
     </header>
   )
 }
