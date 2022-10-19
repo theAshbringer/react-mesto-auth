d6c823ddd54e70bba0b8be46d2e79337f6fdf397
theAsbringer <kristya1310@gmail.com>
2022-10-19 22:45:41 +0700

    ref: smarter submit button component

--- src/shared/SubmitButton/SubmitButton.js
+++ src/shared/SubmitButton/SubmitButton.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './SubmitButton.css'
 
-const SubmitButton = ({ children, className = "", inverted = false }) => {
+const SubmitButton = ({ children, className = "", inverted = false, ...props }) => {
   const btnClassName = `submit-btn ${inverted ? 'submit-btn_inverted' : ''} ${className}`;
 
   return (
     <button
       className={btnClassName}
       type="submit"
+      {...props}
     >
       {children}
     </button>
